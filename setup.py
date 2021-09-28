8b2858474ba2ab7d41c26197fe13247afc5bece3
Kirill <kurdyukov_kirya@mail.ru>
2021-09-28 15:48:59 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='kurdyukov_module',
+    version='0.1.0',    
+    description='A example Python package',
+    author='Kirill Kurdyukov',
+    author_email='kurdyukov_kirya@mail.ru',    
+    license='BSD 2-clause',
+    packages=['kurdyukov_module'],
+    install_requires=[
+                      'numpy',
+                      'matplotlib',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)    
